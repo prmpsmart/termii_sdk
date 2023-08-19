dd7ea87cd1b9b70004acc36c134d1465c4ecb6dc
Miracle Apata <prmpsmart@gmail.com>
2023-08-19 11:00:51 +0100

    version 1.0

--- setup.cmd
+++ setup.cmd
@@ -1,7 +1,7 @@
 @REM pip install setuptools wheel twine
 @REM python setup.py sdist bdist_wheel
-@REM twine upload dist/* --verbose
-twine upload dist/termii_sdk-0.5* --verbose
+twine upload dist/termii_sdk-1.0*
 
-@REM prmpsmart or __token__
-@REM prmpsmart7625# or pypi-AgEIcHlwaS5vcmcCJDFmOWNlOWJiLWQ4YmYtNGE3Mi04OGViLTQ1Njk4M2Q0NDY1NAACElsxLFsidGVybWlpLXNkayJdXQACLFsyLFsiNWM3ODY5ODAtYmYyOC00YjMzLThiYTYtMTAyZjcyN2FiM2Y1Il1dAAAGIC0m-tEXtBMHwO84NMRUdfjA88T6vtFpiE0J3dJD61KC
+
+@REM __token__
+@REM pypi-AgEIcHlwaS5vcmcCJGFhOTJiOTg1LTFjZmUtNDczYi04MTk3LTM3OTA1ZTNjY2EyZAACElsxLFsidGVybWlpLXNkayJdXQACLFsyLFsiNWM3ODY5ODAtYmYyOC00YjMzLThiYTYtMTAyZjcyN2FiM2Y1Il1dAAAGIHj1eFieAtPh4WXt567mkzHYvfgzZnZxlpaglqWEvc0v
