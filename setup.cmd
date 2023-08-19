b8174218d90e9391ff315436a51d9d29fa2a45ca
Miracle Apata <prmpsmart@gmail.com>
2023-08-19 10:44:21 +0100

    pushed the version 0.5 to pypi

--- setup.cmd
+++ setup.cmd
@@ -1,3 +1,7 @@
-@REM pip install setuptools wheel
+@REM pip install setuptools wheel twine
 @REM python setup.py sdist bdist_wheel
-twine upload dist/*
+@REM twine upload dist/* --verbose
+twine upload dist/termii_sdk-0.5* --verbose
+
+@REM prmpsmart or __token__
+@REM prmpsmart7625# or pypi-AgEIcHlwaS5vcmcCJDFmOWNlOWJiLWQ4YmYtNGE3Mi04OGViLTQ1Njk4M2Q0NDY1NAACElsxLFsidGVybWlpLXNkayJdXQACLFsyLFsiNWM3ODY5ODAtYmYyOC00YjMzLThiYTYtMTAyZjcyN2FiM2Y1Il1dAAAGIC0m-tEXtBMHwO84NMRUdfjA88T6vtFpiE0J3dJD61KC
